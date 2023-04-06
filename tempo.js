d062fdc97f88a04dd79c9f64b7252643010a6ee3
lmv7296 <lmv7296@gmail.com>
2023-04-06 12:27:45 -0300

    adding all the changes made from windows

---
+++ tempo.js
@@ -0,0 +1,21 @@
+// speedlimit - 70
+// 5 over speed = 1 point 
+// Math.floor
+// 12 point or above = suspended
+
+
+
+checkSpeed(75)
+
+
+function checkSpeed(speed) {
+     
+     if (speed < 75){
+          console.log(speed);
+          return 'speed';
+     }
+     else if (speed > 75)
+          return 'ja'
+     
+     
+     }
